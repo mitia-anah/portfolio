ee16b6da5589796889a2533fc2bea2945d7e7d9b
Rosny Ratsianompo <rosny.rat@onja.org>
2021-05-21 19:20:14 +0300

    first commit

---
+++ picture/picture.js
@@ -0,0 +1,6 @@
+// import birthday from '../images/birthday';
+// import parade from '../images/parade';
+// import photograf from '../images/photograf';
+// import professional from '../images/professionalme';
+// import quiz from '../images/quiz';
+// import sport from '../images/sport';
